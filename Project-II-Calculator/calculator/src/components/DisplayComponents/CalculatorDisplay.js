da6ffd10c2689585659b3864a24c166d07c38284
zachary-vincent <45057145+ZachieV@users.noreply.github.com>
2019-05-08 20:07:43 -0700

    I think I got everything.
    Just having a little trouble with the compiler

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = (props) => {
+  return <div className = "calc-total" > {
+    props.total
+  } < /div>;
+};
+
+export default CalculatorDisplay;
